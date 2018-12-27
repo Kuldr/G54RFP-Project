63522f6fbff202f131c3b8cbf8dcb5b215ba5dd6
Benjamin Charlton <kuldrgames@gmail.com>
2018-12-27 16:46:02 +0000

    Restructured and added comments to solvers

--- Solvers/HillClimbing.hs
+++ Solvers/HillClimbing.hs
@@ -9,44 +9,42 @@ import Solvers.Naive
 
 
 -- Shared Helper Functions
+-- Generating list of neighbouring solutions
 neighbours :: Solution -> [Solution]
 neighbours (S xs) = [flipNthValue (S xs) i | i <- [0..n]]
                         where n = length xs - 1
-flipNthValue :: Solution -> Int -> Solution
-flipNthValue (S xs) n = S $ take n xs ++ [flipVar (xs!!n)] ++ drop (n + 1) xs
-flipVar :: (Int, Bool) -> (Int, Bool)
-flipVar (i,b) = (i, not b)
 uphillNeighbours :: Problem -> Solution -> [Solution]
 uphillNeighbours p (S xs) = [s | i <- [0..n],
                                         let s = flipNthValue (S xs) i,
                                         evaluateProblem p s > e ]
                         where n = length xs - 1
                               e = evaluateProblem p (S xs)
-
--- Steepest Ascent
---  Evaluate all and then choose best
--- Set up the steepest ascent hill climb
---      This is the function to call at the begining
-setUpSAHC :: Problem -> Int -> Int -> IO Solution
-setUpSAHC p v t =
-    do
-        startingSolution <- randomSolution v -- Starts with random solution
-        return $ steepestAscentHillClimb p startingSolution t
--- Main Loop
-steepestAscentHillClimb :: Problem -> Solution -> Int -> Solution
-steepestAscentHillClimb p s 0 = s
-steepestAscentHillClimb p s t =
-    if next == s then s
-        else steepestAscentHillClimb p next (t-1)
-            where next = steepestAscentHillClimbStep p s
--- Single step of hill climb
-steepestAscentHillClimbStep :: Problem -> Solution -> Solution
-steepestAscentHillClimbStep p s = best $ (evaluateProblem p s, s):(evalutateAllNeighbours p s)
+flipNthValue :: Solution -> Int -> Solution
+flipNthValue (S xs) n = S $ take n xs ++ [flipVar (xs!!n)] ++ drop (n + 1) xs
+flipVar :: (Int, Bool) -> (Int, Bool)
+flipVar (i,b) = (i, not b)
+-- Evaluating neighbouring solutions
 evalutateAllNeighbours :: Problem -> Solution -> [(Int, Solution)]
 evalutateAllNeighbours p s = [(evaluateProblem p n, n) | n <- neighbours s]
 best :: [(Int, Solution)] -> Solution
 best xs = snd $ (sortBy (flip $ comparing fst) xs) !! 0
 
+-- Steepest Ascent Hill Climb
+--  Evaluate all neighbours and then choose the one that makes the most progress
+-- Setup the solver with problem p with v variables and m max steps to take
+--      Can finish early is it can't find an improving solution
+setUpSteepestAscent :: Problem -> Int -> Int -> IO Solution
+setUpSteepestAscent p v m =
+    do
+        startingSolution <- randomSolution v -- Starts with random solution
+        return $ steepestAscent p startingSolution m
+-- Main Loop
+steepestAscent :: Problem -> Solution -> Int -> Solution
+steepestAscent p s 0 = s
+steepestAscent p s m =
+    if next == s then s -- returns early if there are no improving neighbours
+        else steepestAscent p next (m-1)
+            where next = best $ (evaluateProblem p s, s):(evalutateAllNeighbours p s)
 
 -- OTHER HILL CLIMBING MAY IMPLEMENT
 --Simple Hill Climbing
--- Solvers/Naive.hs
+++ Solvers/Naive.hs
@@ -4,12 +4,13 @@ import System.Random
 import Control.Monad
 import CNF.Types
 
+-- Generates a solution w/ v variables that are True or False respecitvely
 allTrueSolution :: Int -> Solution
 allTrueSolution v = S [(i, True) | i <- [1..v]]
-
 allFalseSolution :: Int -> Solution
 allFalseSolution v = S [(i, False) | i <- [1..v]]
 
+-- Generates a solution w/ v variables that are randomly True or False
 randomSolution :: Int -> IO Solution
 randomSolution v = do
                     bs <- replicateM v (randomRIO (True, False))
