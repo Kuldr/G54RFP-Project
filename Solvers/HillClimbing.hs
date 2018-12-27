ed29d718555d9864c421b0f3b67768d1b29b1fde
Benjamin Charlton <kuldrgames@gmail.com>
2018-12-27 17:07:07 +0000

    Added simple hill climbing and modified uphill neighbours

--- Solvers/HillClimbing.hs
+++ Solvers/HillClimbing.hs
@@ -14,11 +14,7 @@ neighbours :: Solution -> [Solution]
 neighbours (S xs) = [flipNthValue (S xs) i | i <- [0..n]]
                         where n = length xs - 1
 uphillNeighbours :: Problem -> Solution -> [Solution]
-uphillNeighbours p (S xs) = [s | i <- [0..n],
-                                        let s = flipNthValue (S xs) i,
-                                        evaluateProblem p s > e ]
-                        where n = length xs - 1
-                              e = evaluateProblem p (S xs)
+uphillNeighbours p s = filter (\n -> evaluateProblem p n > evaluateProblem p s ) $ neighbours s
 flipNthValue :: Solution -> Int -> Solution
 flipNthValue (S xs) n = S $ take n xs ++ [flipVar (xs!!n)] ++ drop (n + 1) xs
 flipVar :: (Int, Bool) -> (Int, Bool)
@@ -30,7 +26,7 @@ best :: [(Int, Solution)] -> Solution
 best xs = snd $ (sortBy (flip $ comparing fst) xs) !! 0
 
 -- Steepest Ascent Hill Climb
---  Evaluate all neighbours and then choose the one that makes the most progress
+--      Evaluate all neighbours and then choose the one that makes the most progress
 -- Setup the solver with problem p with v variables and m max steps to take
 --      Can finish early is it can't find an improving solution
 setUpSteepestAscent :: Problem -> Int -> Int -> IO Solution
@@ -46,9 +42,24 @@ steepestAscent p s m =
         else steepestAscent p next (m-1)
             where next = best $ (evaluateProblem p s, s):(evalutateAllNeighbours p s)
 
+-- Simple Hill Climbing
+--      Choose the first neighbour that improves
+--      Due to evaluating all neighbours this implementation isn't faster than
+--          steepest ascent. Benefits can be found with greater exploration though
+-- Setup the solver with problem p with v variables and m max steps to take
+setUpSimpleHC :: Problem -> Int -> Int -> IO Solution
+setUpSimpleHC p v m =
+    do
+        startingSolution <- randomSolution v -- Starts with random solution
+        return $ simpleHC p startingSolution m
+simpleHC :: Problem -> Solution -> Int -> Solution
+simpleHC p s 0 = s
+simpleHC p s m =
+    if next == s then s -- returns early if there are no improving neighbours
+        else simpleHC p next (m-1)
+            where next = head $ (uphillNeighbours p s) ++ [s]
+
 -- OTHER HILL CLIMBING MAY IMPLEMENT
---Simple Hill Climbing
---  Choose the first neighbour that improves
 --Stochastic
 --  Select Random neighbour and evaluate
 --  Use that if it makes an improvement
