a765edbc6c4b1e6f42c0ee42e145fdc0af181dfb
Benjamin Charlton <kuldrgames@gmail.com>
2018-12-27 19:19:47 +0000

    Added bitFlip mutation

--- Solvers/GeneticAlgorithm.hs
+++ Solvers/GeneticAlgorithm.hs
@@ -3,6 +3,7 @@ module Solvers.GeneticAlgorithm where
 import Control.Monad
 import System.Random
 import Solvers.Naive
+import Solvers.HillClimbing
 import CNF.Types
 import CNF.Evaluator
 
@@ -13,7 +14,7 @@ import CNF.Evaluator
         -- Loop on children to gen
         -- Select Parents
         -- Crossover
-        -- Mutation
+        -- Mutation -- Small chance of changing
         -- Selection of new children
 
 initialisePopulation :: Int -> Int -> IO [Solution]
@@ -36,8 +37,20 @@ uniformHelper (m:ms) (d:ds) = do
     if randomBool then return $ m:restCrossover
                   else return $ d:restCrossover
 
+bitFlipMutation :: Solution -> Int -> IO Solution
+-- Note can potentially flip a bit and flip it back if given enough bits to flip
+bitFlipMutation s 0 = do return s
+bitFlipMutation (S xs) n = do
+    randomIndex <- randomRIO (0, (length xs - 1))
+    let mutation = flipNthValue (S xs) randomIndex
+    ans <- bitFlipMutation mutation (n-1)
+    return $ ans
 
 -- Other Crossover methods
 --      Single Point Crossover
 --      K Point Crossover
 --      2 Point Crossover
+
+-- Other Mutation methods
+--      Bit String Mutation (1/l chance of flipping per bit)
+--      Swap 2 Bits
