dcb516c2d4add8c5487d22e757b821b74fba0a3f
Benjamin Charlton <kuldrgames@gmail.com>
2019-01-15 18:38:54 +0000

    Added Naive solvers section

--- Report/reportIIICode.hs
+++ Report/reportIIICode.hs
@@ -7,3 +7,13 @@ newtype Var     = V Int
 --2.3
 newtype Solution = S [(Int, Bool)] deriving (Show, Eq)
 evaluateProblem :: Problem -> Solution -> Int
+
+--2.4.1
+allTrueSolution, allFalseSolution :: Int -> Solution
+allTrueSolution  v = S [(i, True)  | i <- [1..v]]
+allFalseSolution v = S [(i, False) | i <- [1..v]]
+
+--2.4.2
+randomSolution :: Int -> IO Solution
+randomSolution v = do bs <- replicateM v (randomRIO (True, False))
+                      return $ S $ zip [1..v] bs
